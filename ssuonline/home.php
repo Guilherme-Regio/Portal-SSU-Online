69250770a86dbb4709ccfa74e42dcf0bb22e0d9c
Guilherme Regio <guilhermeregio02@gmail.com>
2022-03-19 15:45:24 -0300

    Update home.php

--- ssuonline/home.php
+++ ssuonline/home.php
@@ -5,7 +5,6 @@ require_once 'html.php';
 <br></br>
 <br></br>
 <br></br>
-<br></br>
 <h1 style="font-family: vag-rounded-bold; color: #3a3f48; font-size: 4.5em; text-align: center;">Bem vindo, <?php echo $_SESSION["user"] ?>!!!</h1>
 <p class="span12 text-center">SSU Online.</p>
 <?php include("footer.php"); ?>
